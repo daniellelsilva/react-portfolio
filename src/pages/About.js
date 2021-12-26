64d64699562638dae07844f5c03f47d605bf27b0
Danielle Silva <danielle.luisasilva@gmail.com>
2021-12-26 15:16:05 -0300

    finaliza home

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import image from '../assets/IMG_20201009_152848.jpg'
+import './about.css';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>renderizou about</p>
+      <div className="about-page">
+        <img src={image} alt="profile" />
+        <p>teste</p>
       </div>
     )
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import imagem from '../undraw_stars_re_6je7.svg';
+import imagem from '../assets/undraw_add_color_re_buro.svg';
 import './home.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home-div">
-        <section className="text-section slide-right">
+        <section className="text-section text-animation">
           <p>Olá,</p>
-          <p>bem vindo(a) ao meu portfólio</p>
+          <p>Sou a Danielle Silva</p>
+          <p>Estudante de Desenvolvimento Web | Trybe </p>
         </section>
-        <section className="img-section slide-right">
+        <section className="img-section text-animation">
           <img src={imagem} alt="bla"/>
         </section>
       </div>
