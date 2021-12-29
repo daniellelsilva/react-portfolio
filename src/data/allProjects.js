2c465e139f08a725e9f3055acbec1d35268b7683
Danielle Silva <danielle.luisasilva@gmail.com>
2021-12-29 19:26:34 -0300

    adiciona popups

--- src/data/allProjects.js
+++ src/data/allProjects.js
@@ -6,61 +6,84 @@ import toDoList from '../assets/to-do-list-video.mp4';
 import pixelArt from '../assets/pixel-art-video.mp4';
 import trybewarts from '../assets/trybewarts-video.mp4';
 
+import imgTrybetunes from '../assets/img-trybetunes.png';
+import imgTryunfo from '../assets/img-tryunfo.png';
+import imgSolarSystem from '../assets/img-solar-system.png';
+import imgShoppingCart from '../assets/img-shopping-cart.png';
+import imgToDoList from '../assets/img-to-do-list.png';
+import imgPixelArt from '../assets/img-pixel-art.png';
+import imgTrybewarts from '../assets/img-trybewarts-img.png';
+
+
 const allProjects = [
   {
     id: 7,
     name: 'TrybeTunes',
-    linkVideo: trybetunes,
+    img: imgTrybetunes,
     linkGitHub: 'https://github.com/daniellelsilva/trybetunes',
     linkPage: 'https://daniellelsilva.github.io/trybetunes/#/',
+    linkVideo: trybetunes,
+    text: 'Aproposta do projeto foi criar o TrybeTunes, uma aplicação capaz de reproduzir músicas das mais variadas bandas e artistas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada.'
   },
 
   {
     id: 6,
     name: 'Tryunfo',
+    img: imgTryunfo,
     linkVideo: tryunfo,
     linkGitHub: 'https://github.com/daniellelsilva/tryunfo',
     linkPage: 'https://daniellelsilva.github.io/tryunfo',
+    text: 'Neste projeto foi desenvolvido um jogo de Super Trunfo. A aplicação possuia tema livre e deveria ser desenvolvido um baralho, sendo possível criar cartas, pré-visualizar a carta, adicionar e remover cartas do baralho, além de possuir três filtros.'
   },
 
   {
     id: 5,
     name: 'Solar system',
+    img: imgSolarSystem,
     linkVideo: solarSystem,
     linkGitHub: 'https://github.com/daniellelsilva/solar-system',
     linkPage: 'https://daniellelsilva.github.io/solar-system/',
+    text: 'Este foi o primeiro projeto do módulo de Front-end e meu primeiro projeto em React, foi desenvolvido um modelo do sistema solar, sendo visualizado todos os planetas e missões espaciais.'
   },
 
   {
     id: 4,
     name: 'Shopping cart',
+    img: imgShoppingCart,
     linkVideo: shoppingCart,
     linkGitHub: 'https://github.com/daniellelsilva/shopping-cart',
     linkPage: 'https://daniellelsilva.github.io/shopping-cart/',
+    text: 'Primeiro projeto utilizando API! O objetivo do projeto foi desenvolver um carrinho de compras totalmente dinâmico, consumindo dados diretamente de uma API.'
   },
 
   {
     id: 3,
     name: 'To do list',
+    img: imgToDoList,
     linkVideo: toDoList,
     linkGitHub: 'https://github.com/daniellelsilva/to-do-list',
     linkPage: 'https://daniellelsilva.github.io/to-do-list/',
+    text: 'Projeto do bloco de fundamentos, foi utilizado CSS e Javascript para criar uma aplicação onde é possível adicionar e remover tarefas, marcar tarefa como feita, remover somente itens marcados, salvar a lista e remover todos os itens.'
   },
 
   {
     id: 2,
     name: 'Pixel-art',
+    img: imgPixelArt,
     linkVideo: pixelArt,
     linkGitHub: 'https://github.com/daniellelsilva/pixel-art',
     linkPage: 'https://daniellelsilva.github.io/pixel-art/',
+    text: 'Projeto do bloco de fundamentos, foi utilizada manipulação de DOM e Javascript para criar uma aplicação onde é possível fazer arte com pixels. As cores deveriam ser aleatórias e o quadro deve permitir mudar o número de pixels.'
   },
 
   {
     id: 1,
     name: 'Trybewarts',
+    img: imgTrybewarts,
     linkVideo: trybewarts,
     linkGitHub: 'https://github.com/daniellelsilva/trybewarts',
     linkPage: 'https://daniellelsilva.github.io/trybewarts/',
+    text: 'Projeto do bloco de fundamentos, o desafio era criar um formulário HTML para a escola de magia Trybewarts utilizando CSS flexbox e Javascript. O projeto foi desenvolvido em dupla com Nayara Vasconcelos.'
   }
 ];
 
--- src/pages/IndividualProject.js
+++ src/pages/IndividualProject.js
@@ -2,22 +2,64 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class IndividualProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      popup: false,
+    }
+
+    this.showPopup = this.showPopup.bind(this);
+    this.hidePopup = this.hidePopup.bind(this);
+  }
+
+  showPopup() {
+    this.setState({ popup: true })
+  }
+
+  hidePopup() {
+    this.setState({ popup: false })
+  }
+
   render() {
-    const { name, linkVideo, linkGitHub, linkPage } = this.props.project;
+    const { name, img, linkGitHub, linkPage, linkVideo, text } = this.props.project;
+    const { popup } = this.state
     return (
       <div className='proj-individual-videos'>
-        <video src={linkVideo} controls="controls" />
+        <img className='imagemm' src={img} />
         <p>{name}</p>
           <div className="div-video-icons">
-            <i className="bi bi-info-square video-info-icon"></i>
+            <button onClick={this.showPopup} className="more-info-button">
+              <i className="bi bi-plus-circle video-info-icon"></i>
+              {/* <i className="bi bi-info-square video-info-icon"></i> */}
+            </button>
             <a href={linkGitHub} target="_blank" rel="noopener noreferrer">
               <i className="bi bi-github video-github-icon"></i>
             </a>
             <a href={linkPage} target="_blank" rel="noopener noreferrer">
-              <i className="bi bi-tv video-page-icon"></i>
+              <i className="bi bi-eye video-page-icon"></i>
             </a>
           </div>
+          {popup ? (
+            <section className='container-more-info'>
+              <div className='text-video-container'>
+                <div className='div-close-popup'>
+                  <button className='popupClose' onClick={this.hidePopup}><i className="bi bi-x-circle popupClose"></i></button>
+                </div>
+                
+                <video src={linkVideo} controls="controls" />
+
+                <div className='project-informative-text'>
+                  <p>{name}</p>
+                  <p className='info-project-text'>{text}</p>
+                </div>
+                
+              </div>
+            </section>
+          ) : null}
       </div>
+
+      
     )
   }
 }
@@ -25,9 +67,11 @@ export default class IndividualProject extends Component {
 IndividualProject.propTypes = {
   project: PropTypes.shape({
     name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
     linkVideo:PropTypes.string.isRequired,
     linkGitHub:PropTypes.string.isRequired,
     linkPage:PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
   })
   
 }
