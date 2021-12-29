0037db84a55c5255993c5e54365968539e706fb6
Danielle Silva <danielle.luisasilva@gmail.com>
2021-12-28 21:26:17 -0300

    customizacao videos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,53 +13,114 @@ export default class Projects extends Component {
   render() {
     return (
       <div className="project-div">
-        <section>
+        <section className="proj-section-introduction">
           <p>Projetos</p>
-          <p>Meus projetos estão organizados em ordem cronológica</p>
+          <p>Os projetos estão organizados em ordem cronológica</p>
         </section>
 
-        <section>
-          <div>
+        <section className='projects-video-section'>
+          <div className='proj-individual-videos'>
             <video src={trybetunes} width="600" height="300" controls="controls" />
             <p>TrybeTunes</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/trybetunes" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/trybetunes/#/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+              
+            </div>
           </div>
             
-          <div>
-            <video src={tryunfo} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={tryunfo} controls="controls" />
             <p>Tryunfo</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/tryunfo" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/tryunfo" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <video src={solarSystem} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={solarSystem} controls="controls" />
             <p>Solar system</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/solar-system" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/solar-system/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <video src={shoppingCart} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={shoppingCart} controls="controls" />
             <p>Shopping cart</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/shopping-cart" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/shopping-cart/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <video src={toDoList} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={toDoList} controls="controls" />
             <p>To do list</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/to-do-list" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/to-do-list/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <video src={pixelArt} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={pixelArt} controls="controls" />
             <p>Pixel-art</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/pixel-art" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              <a href="https://daniellelsilva.github.io/pixel-art/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
-          <div>
-            <video src={trybewarts} width="600" height="300" controls="controls" />
+          <div className='proj-individual-videos'>
+            <video src={trybewarts} controls="controls" />
             <p>Trybewarts</p>
+            <div className="div-video-icons">
+              <i className="bi bi-info-square video-info-icon"></i>
+              <a href="https://github.com/daniellelsilva/trybewarts" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-github video-github-icon"></i>
+              </a>
+              
+              <a href="https://daniellelsilva.github.io/trybewarts/" target="_blank" rel="noopener noreferrer">
+                <i className="bi bi-tv video-page-icon"></i>
+              </a>
+            </div>
           </div>
 
         </section>
         
-
-        {/* <video src={video} width="600" height="300" controls="controls" autoPlay="true" /> */}
-        
-        <a href='https://daniellelsilva.github.io/to-do-list/' target="_blank" rel="noopener noreferrer">aqui</a>
       </div>
     )
   }
