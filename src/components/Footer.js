acfa89ff4f62c5de54ba7eaa9af6b00c3c9d961d
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-03 22:20:35 -0300

    adiciona variaveis de cor ao header e footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './footer.css';
+import './footer.scss';
 
 class Footer extends React.Component {
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import './header.css';
+import './header.scss';
 
 class Header extends React.Component {
   render() {
