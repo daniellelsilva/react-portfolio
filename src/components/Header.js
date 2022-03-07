773ec41be5b4a84e1a66cee0437ad5e00dd385e1
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-07 00:32:14 -0300

    muda header para scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,18 @@ import './header.scss';
 class Header extends React.Component {
   render() {
     return (
-    <header>
-      <div className="botao-teste">
-        <Link exact to="/" className="portfolio-name">
-          <button className="icon-btn add-btn">
-            <div className="btn-txt">Danielle</div>
+    <header className="header">
+      <div className="header-btn">
+        <Link exact to="/" className="header-portfolio-name">
+          <button className="header-icon-btn header-link-btn">
+            <div className="header-btn-txt">
+              Danielle
+            </div>
           </button>
         </Link>
       </div>
 
-      <nav>
+      <nav className="header-nav">
         <NavLink exact to="/" activeClassName="selected">Home</NavLink>
         <NavLink to="/about" activeClassName="selected">Sobre</NavLink>
         <NavLink to="/projects" activeClassName="selected">Projetos</NavLink>
