b52df170db36d189af64aa4b4ca1c6246ce45508
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-13 22:18:14 -0300

    inicia componente popup - ainda não implementado

---
+++ src/components/Popup.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// export default function Popup() {
+//   return (
+//     <section className='popup'>
+//             <div className='popup-text-video-container'>
+//               <div className='popup-div-close'>
+//                 <button className='popup-Close' onClick={hidePopup}><i className="bi bi-x-circle popupClose"></i></button>
+//               </div>
+              
+//               <video src={linkVideo} controls="controls" />
+
+//               <div className='popup-informative-text'>
+//                 <p>{name}</p>
+//                 <p>{ moduleCourse }</p>
+//                 <p className='popup-info-project-text'>{text}</p>
+//               </div>
+              
+//             </div>
+//           </section>
+//   )
+// }
