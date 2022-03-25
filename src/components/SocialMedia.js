52f453cbdf417d83a1cae3e9f2171f78b666f080
Danielle Silva <danielle.luisasilva@gmail.com>
2022-03-25 19:03:34 -0300

    modifica logica de posicionamento de elementos no home e componente social media

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,7 +5,7 @@ import '../styles/components/socialMedia.scss';
 
 export default function SocialMedia() {
   return (
-    <section className="home-links">
+    <section className="social-media">
       <a href="https://www.linkedin.com/in/danielle-lsilva/" target="_blank" rel="noopener noreferrer">
         <Icon icon="akar-icons:linkedin-fill" />
       </a>
@@ -14,6 +14,10 @@ export default function SocialMedia() {
       </a>
       <a href="https://www.instagram.com/ls_danielle/" target="_blank" rel="noopener noreferrer">
         <Icon icon="bi:instagram" />
+      </a>
+
+      <a href="https://www.instagram.com/ls_danielle/" target="_blank" rel="noopener noreferrer" className="mail-icon">
+        <Icon icon="ic:baseline-email" />
       </a>
     </section>
   )
