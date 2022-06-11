b0c43ebfb1aac5e7266cdf3df9535cab70f11deb
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-10 21:46:03 -0300

    new icons to about

--- src/data/hardSkillsIcons.js
+++ src/data/hardSkillsIcons.js
@@ -1,67 +1,89 @@
 const hardSkillsIcons = [
   {
     id: 1,
-    icon: 'fontisto:css3',
+    icon: 'akar-icons:react-fill',
     className: 'about-icon',
-    title: 'CSS' 
+    title: 'ReactJS' 
   },
-
   {
     id: 2,
     icon: 'cib:javascript',
     className: 'about-icon',
     title: 'JavaScript' 
   },
-
   {
     id: 3,
     icon: 'akar-icons:sass-fill',
     className: 'about-icon',
     title: 'Sass' 
   },
-
   {
     id: 4,
+    icon: 'fontisto:css3',
+    className: 'about-icon',
+    title: 'CSS' 
+  },
+  {
+    id: 5,
+    icon: 'akar-icons:bootstrap-fill',
+    className: 'about-icon',
+    title: 'Bootstrap' 
+  },
+  {
+    id: 6,
     icon: 'icomoon-free:html-five',
     className: 'about-icon',
     title: 'HTML' 
   },
-
   {
-    id: 5,
-    icon: 'akar-icons:react-fill',
+    id: 7,
+    icon: 'akar-icons:redux-fill',
     className: 'about-icon',
-    title: 'ReactJS' 
+    title: 'Redux' 
   },
-
   {
-    id: 6,
+    id: 8,
     icon: 'bi:git',
     className: 'about-icon',
     title: 'Git' 
   },
-
   {
-    id: 7,
+    id: 9,
     icon: 'fontisto:github',
     className: 'about-icon',
     title: 'GitHub' 
   },
 
   {
-    id: 8,
+    id: 10,
     icon: 'simple-icons:jest',
     className: 'about-icon',
     title: 'Jest' 
   },
-
   {
-    id: 9,
+    id: 11,
     icon: 'fontisto:mysql',
     className: 'about-icon',
     title: 'MySQL' 
   },
-
+  {
+    id: 12,
+    icon: 'file-icons:docker',
+    className: 'about-icon',
+    title: 'Docker' 
+  },
+  {
+    id: 13,
+    icon: 'bxl:nodejs',
+    className: 'about-icon',
+    title: 'NodeJS' 
+  },
+  {
+    id: 14,
+    icon: 'cib:typescript',
+    className: 'about-icon',
+    title: 'Typescript' 
+  },
 ];
 
 export default hardSkillsIcons;
