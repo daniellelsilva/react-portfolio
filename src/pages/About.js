e926c3d4d3e15ef139624df93a710a3888aeeb53
Danielle Silva <danielle.luisasilva@gmail.com>
2022-09-18 19:27:46 -0300

    change social media to middle of screen

--- src/pages/About.js
+++ src/pages/About.js
@@ -55,7 +55,7 @@ export default class About extends Component {
             
           </div>
           
-          <SocialMedia className="testando" />
+          <SocialMedia />
         </section>
         
       </div>
