e858d26257a79f77e3df521833f0913c6ad9e140
Danielle Silva <danielle.luisasilva@gmail.com>
2022-09-18 21:42:46 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import React from 'react';
 import ReactGA from 'react-ga4';
-import { Route, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Home from '../src/pages/Home';
 import About from '../src/pages/About';
@@ -11,11 +11,11 @@ ReactGA.send("pageview");
 
 function App() {
   return (
-    <Routes>
+    <>
       <Route exact path="/" component={ Home } />
       <Route path="/about" component={ About } />
       <Route path="/projects" component={ Projects } />    
-    </Routes>
+    </>
   );
 }
 
